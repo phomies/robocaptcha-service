0f72affd38ad523e8127fa9f6295b0501a093e22
Wen Liang Goh <wlgoh.2020@scis.smu.edu.sg>
2022-03-02 18:41:15 +0800

    Notifications and call history done

---
+++ main/sqs.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sqs"
+	"os"
+)
+
+func sqsSendNotification(messageBody string) error {
+	sqsClient := sqs.New(sqsGetSession())
+	queueUrl := os.Getenv("AWS_SQS_NOTIFICATION_URL")
+	fmt.Println(sqsClient.Client.Endpoint)
+
+	fmt.Printf("Sending notification")
+
+	_, err := sqsClient.SendMessage(&sqs.SendMessageInput{
+		QueueUrl:    &queueUrl,
+		MessageBody: aws.String(messageBody),
+	})
+
+    fmt.Println(err)
+
+	return err
+}
+
+func sqsGetSession() *session.Session {
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		Config: aws.Config{
+			Region: aws.String("ap-southeast-1"),
+			Credentials: credentials.NewStaticCredentials(
+				os.Getenv("AWS_ID"),
+				os.Getenv("AWS_SECRET"),
+				"",
+			),
+		},
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+	return sess
+}
